601d968dbef1fe4a1ebea83daca088afcdb1ae0a
Raed <alyazan9594@gmail.com>
2025-11-13 17:24:33 +0400

    fix: Use Cloud SQL connector to connect to the database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,24 @@
 const { Pool } = require('pg');
+const { Connector } = require('@google-cloud/cloud-sql-connector');
+
+const connector = new Connector();
 
 const pool = new Pool({
+  // The Cloud SQL JS Connector returns a new promise on every invocation,
+  // so we need to wrap it in a function that returns the same promise.
+  stream: () => {
+    if (pool.streamPromise) {
+      return pool.streamPromise;
+    }
+    pool.streamPromise = connector.getStream({
+      instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
+      ipType: 'PUBLIC', // or 'PRIVATE'
+    });
+    return pool.streamPromise;
+  },
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = {
