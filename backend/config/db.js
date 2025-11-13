93707ff641533d999ec8bd5f0291c185417e08d7
Raed <alyazan9594@gmail.com>
2025-11-13 19:26:02 +0400

    fix: Use correct method to get Cloud SQL connection options

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,24 +3,27 @@ const { Connector } = require('@google-cloud/cloud-sql-connector');
 
 const connector = new Connector();
 
-const pool = new Pool({
-  // The Cloud SQL JS Connector returns a new promise on every invocation,
-  // so we need to wrap it in a function that returns the same promise.
-  stream: () => {
-    if (pool.streamPromise) {
-      return pool.streamPromise;
-    }
-    pool.streamPromise = connector.getStream({
-      instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
-      ipType: 'PUBLIC', // or 'PRIVATE'
-    });
-    return pool.streamPromise;
-  },
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const getPool = async () => {
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
+    ipType: 'PUBLIC',
+  });
+  const pool = new Pool({
+    ...clientOpts,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+  return pool;
+};
+
+let pool;
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  query: async (text, params) => {
+    if (!pool) {
+      pool = await getPool();
+    }
+    return pool.query(text, params);
+  },
 };
